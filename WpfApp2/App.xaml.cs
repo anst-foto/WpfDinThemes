681cd051d9e8a48a5ebbceb99a2c831c4dd0b312
Starinin Andrey <starininandrey@gmail.com>
2024-09-19 19:11:53 +0300

    Update

--- WpfApp2/App.xaml.cs
+++ WpfApp2/App.xaml.cs
@@ -1,13 +1,44 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.IO;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace WpfApp2;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-}
+    public static App? Instance;
+    public static string? StylesDirectory;
+    public static IEnumerable<string>? Files;
 
+    public App()
+    {
+        Instance = this;
+        
+        StylesDirectory = Path.Combine(
+            path1: Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)!, 
+            path2: "Styles");
+        
+        Files = Directory
+            .GetFiles(StylesDirectory!)
+            .Select(f => new FileInfo(f))
+            .Select(fi => fi.Name.Replace(".xaml", ""));
+        var fileName = Path.Combine(StylesDirectory!, $"{Files!.First()}.xaml");
+        LoadStyleDictionaryFromFile(fileName);
+    }
+    
+    public void LoadStyleDictionaryFromFile(string fileName)
+    {
+        if (!File.Exists(fileName)) return;
+        try
+        {
+            using var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            var dic = (ResourceDictionary)XamlReader.Load(fs);
+            Resources.Clear();
+            Resources.MergedDictionaries.Add(dic);
+        }
+        catch
+        {
+            // ignored
+        }
+    }
+}
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,23 +1,37 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp2;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        foreach (var file in App.Files!)
+        {
+            var menuItem = new MenuItem
+            {
+                Header = file
+            };
+            menuItem.Click += MenuItem_Style_Click;
+            
+            MenuItemStyles.Items.Add(menuItem);
+        }
+    }
+    
+    private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+ 
+    private void MenuItem_Style_Click(object sender, RoutedEventArgs e)
+    {
+        var mi = sender as MenuItem;
+        var fileName = Path.Combine(App.StylesDirectory!, $"{mi!.Header}.xaml");
+        App.Instance!.LoadStyleDictionaryFromFile(fileName);
     }
 }
